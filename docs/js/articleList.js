f9be740940a148cc960000d2ef64416d6ae3fcd3
CYF <914621447@qq.com>
2019-09-04 14:19:26 +0800

    文章2.1 文章2

--- docs/js/articleList.js
+++ docs/js/articleList.js
@@ -7,4 +7,16 @@ $(function () {
             $("#mainContent").load("template/" + href.slice(2));
         }
     });
+
+    $('.ownJump').on('click', function() {
+        event.preventDefault(); // 阻止默认事件
+        event.stopPropagation(); // 阻止冒泡
+        href = $(this).attr("href");
+        if (href) {
+            $(this).parent('.article-box').load("template/" + href.slice(2));
+        }
+    });
+    $('.ownJump').each(function() {
+        $(this).trigger("click");
+    });
 });
