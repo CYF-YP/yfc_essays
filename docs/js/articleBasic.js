d1ccdf743364d76e691eb01002e95d12c805692e
CYF <914621447@qq.com>
2019-09-06 15:13:35 +0800

    文章2.0 markdown

--- docs/js/articleBasic.js
+++ docs/js/articleBasic.js
@@ -1,12 +1,12 @@
 
 function mdSwitch(element) {
-    debugger;
     var converter = new showdown.Converter(),
         text = $($(element).prev(".md-content")[0]).val(),
         html = converter.makeHtml(text);
     $(element).html(html);
 }
 
+$('.md-area').off('click');
 $('.md-area').on("click", function () {
     mdSwitch($(this));
 });
