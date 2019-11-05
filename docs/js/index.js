8fbf74b17d4c13f5bdb287453003ae280eb7abf4
CYF <914621447@qq.com>
2019-11-05 14:12:41 +0800

    Revert "2.4 背景图片预加载"
    This reverts commit ee2b504a82d9255a890d8a3608395d9e99853d9f.
    
    # Conflicts:
    #	docs/css/index.css

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,12 +1,5 @@
-function loadImg() {
-    var bgImage = new Image();
-    bgImage.src = "./img/bg.jpg";    
-}
-
 window.onload = function () {
     // var vConsole = new VConsole();
-    $('.bg').css("background-image","url(" + loadImg() +")");
-
     $('#navHome').on('click', function () {
         $("#mainContent").load("template/articleList.html");
     });
